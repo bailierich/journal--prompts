b33e4faba042f74b51f1432da9eefbb12ef19e76
BRich <bailie.richard@gmail.com>
2023-02-21 23:16:18 -0500

    edited json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 let PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ prompt: getRandomPrompt() });
+  res.json(getRandomPrompt());
 });
 
 const server = app.listen(PORT, () =>
